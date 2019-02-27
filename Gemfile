31d239186a3fb3d5991bd43b9db1ce92c3aaceed
Milena Gaigne <milena.gaigne@gmail.com>
2019-02-27 12:34:46 +0100

    no needs pundit do it like a pork buts it works

--- Gemfile
+++ Gemfile
@@ -9,7 +9,6 @@ gem 'puma'
 gem 'rails', '5.2.2'
 gem 'redis'
 gem 'devise'
-gem 'pundit'
 
 gem 'autoprefixer-rails'
 gem 'bootstrap-sass', '~> 3.3'
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def current_user
+    current_student || current_teacher
+  end
 end
