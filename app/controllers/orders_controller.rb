5103d34332bde844c50ffe08e5c2903b2a7b534d
louise grosgogeat <louise.grosgogeat@gmail.com>
2019-03-01 17:02:03 +0100

    payment

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -8,11 +8,22 @@ class OrdersController < ApplicationController
   # end
 
   def new
-    @order = Order.new
-    # ai je besoin de specility.id ?
-    # si oui, alors il faut nester la route du order/new (voir routes.rb)
+    @order = Order.new(speciality_id: params[:speciality_id])
+     session[:params_session] = {
+                                  :speciality_id => params[:speciality_id]
+                                }
+                                # appeller sous forme de string
   end
 
   def create
+    @order = Order.new
+    @order.speciality_id = session[:params_session]["speciality_id"].to_i
+    @order.student_id = current_student.id
+    @order.status = "payed"
+    if @order.save!
+      redirect_to speciality_path(session[:params_session]["speciality_id"].to_i)
+    else
+      render :new
+    end
   end
 end
