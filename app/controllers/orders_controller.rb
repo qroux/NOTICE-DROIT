c9db11a0eaa5c05bedbe2f6e09d4cc2853393278
louise grosgogeat <louise.grosgogeat@gmail.com>
2019-03-01 14:33:11 +0100

    orders

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,8 +1,16 @@
 class OrdersController < ApplicationController
-  def index
-  end
+  # def index
+  #   @orders = Order.all
+  # end
+
+  # def show
+  #   @order = Order.new()
+  # end
 
   def new
+    @order = Order.new
+    # ai je besoin de specility.id ?
+    # si oui, alors il faut nester la route du order/new (voir routes.rb)
   end
 
   def create
